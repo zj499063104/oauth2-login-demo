2b759c2b1df9ba8bc312969cb2e313705d8f84c7
cang1xiao <499063104@qq.com>
2024-03-09 21:55:43 +0800

    尚硅谷SpringSecurity和OAuth2实战精讲

---
+++ src/main/java/com/example/demo/Oauth2LoginDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2LoginDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2LoginDemoApplication.class, args);
+	}
+
+}
